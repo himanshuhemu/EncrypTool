003eb00db50da80853f3fa546c5bcd894f1d4ba2
himanshu <himanshuk26698@gmail.com>
2018-07-02 22:49:27 +0530

    Improved UI

--- myapp/form2.py
+++ myapp/form2.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class  formClass(forms.Form):
-    en_text = forms.CharField()
-    key2 =  forms.CharField()
+    Encrypted_Text = forms.CharField()
+    The_key =  forms.CharField()
     
